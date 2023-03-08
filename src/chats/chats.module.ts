1c0b5d278f5e14e08aa1e427488c1738978d9d4f
Tywael <yves.althaus42@gmail.com>
2023-03-08 20:19:12 +0000

    wip

--- src/chats/chats.module.ts
+++ src/chats/chats.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { ChatsService } from './chats.service';
 import { ChatsController } from './chats.controller';
 import { PrismaService } from '../prisma.service';
-import { JwtStrategy } from "../auth/jwt.strategy";
+import { UsersModule } from 'src/users/users.module';
+import { AuthMiddleware } from 'src/users/users.middleware';
+import { AuthService } from 'src/auth/auth.service';
+import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
+    imports: [UsersModule],
     controllers: [ChatsController],
-    providers: [ChatsService, PrismaService, JwtStrategy]
+    providers: [ChatsService, PrismaService, AuthMiddleware, AuthService, UsersService, JwtService]
 })
 export class ChatsModule {}
