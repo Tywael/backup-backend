5976529a60d648203e90bf162d06ef06dd4942d0
yace1 <yacinbent@gmail.com>
2023-03-11 01:44:15 +0000

    feat(game-crud): create game working

--- src/games/games.service.ts
+++ src/games/games.service.ts
@@ -1,57 +1,56 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
-import { Game, GameStatus } from '@prisma/client';
+import { Game, GameStatus, UserGame } from '@prisma/client';
 
 @Injectable()
 export class GamesService {
   remove: any;
   constructor(private prisma: PrismaService) {}
 
-  async create( userId: string): Promise<Game | null> {
-    let game = await this.prisma.game.create({
+  async create( userId: string): Promise<Game | void> {
+  let game = await this.prisma.game.create({
 	  data: {
 		status: GameStatus.WAITING,
 	  },
 	})
-	.then((game) => {
-		this.prisma.userGame.create({
-			data: {
-				userId,
-				gameId: game.id,
-			},
-		}).catch((err) => {
-			console.log(err);
-			// should throw new BadRequestException('User not found.'); instead
-			this.deleteGame(game.id);
-			return null;
-		});
-		
-	}) 
+  .then((game) => {
+    this.createUserGame(game.id, userId);
+    return game;
+  })
 	.catch((err) => {
 	  console.log(err);
-	  // should throw new BadRequestException('User not found.'); instead
-	  return null;
+    throw new BadRequestException(err);
 	});
-	console.log(game)
-    return game;
+
+  return game;
 }
 
-async updateGameStatus(gameId: string, status: GameStatus): Promise<Game | null> {
+async createUserGame(gameId: string, userId: string): Promise<UserGame | void> {
+  return await this.prisma.userGame.create({
+    data: {
+    userId: userId,
+    gameId: gameId,
+    },
+  }).catch((err) => {
+    console.log(err);
+    throw new BadRequestException(err);
+  });
+}
+
+async updateGameStatus(gameId: string, status: GameStatus): Promise<Game | void> {
 	const game = await this.prisma.game.update({
 		where: { id: gameId },
 		data: {  status },
     }).catch((err) => {
 		console.log(err);
-		// should throw new BadRequestException(err); instead of returning null
-		return null;
+    throw new BadRequestException(err);
 	});
     return game;
 }
 
-  async getAllGames(): Promise<Game[] | null> {
+  async getAllGames(): Promise<Game[] | void> {
     const games = await this.prisma.game.findMany().catch((err) => {
-		console.log(err);
-		return null;
+    throw new BadRequestException(err);
 	});
 	return games;
   }
@@ -61,28 +60,20 @@ async updateGameStatus(gameId: string, status: GameStatus): Promise<Game | null>
   }
 
   // new user join a game
-  async joinGame(gameId: string, data: Game, userId: string): Promise<Game | null> {
-	if (data.status == GameStatus.FINISHED || data.status == GameStatus.INPROGRESS)
-	  return null;
+  async joinGame(gameId: string, userId: string): Promise<Game | null> {
+    const game = await this.prisma.game.findUnique({ where: { id: gameId } });
+    if (game.status == GameStatus.FINISHED || game.status == GameStatus.INPROGRESS)
+      return null;
 
-	await this.prisma.userGame.create({
-	  data: {
-		userId: userId,
-		gameId: gameId,
-	  },
-	}).catch((err) => {
-	  console.log(err);
-	  return null;
-	});
+    this.createUserGame(gameId, userId);
 
     return await this.prisma.game.update({
-	  where: { id: gameId },
-	  data: {  status: GameStatus.INPROGRESS },
-	}).catch((err) => {
-		console.log(err);
-		return null;
-		// TODO: delete userGame
-	});
+      where: { id: gameId },
+      data: {  status: GameStatus.INPROGRESS },
+    })
+    .catch((err) => {
+      throw new BadRequestException(err);
+    });
   }
 
   async deleteGame(gameId: string): Promise<Game | null> {
